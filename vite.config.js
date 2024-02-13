50094b81ec1d27cbcfc71af328e8c5194531b71c
Nathan Brown <nathanejbrown@gmail.com>
2024-02-13 13:42:32 -0700

    updates pretty much everything so that it actually, you know, works

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,37 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+// import { resolve } from "path";
+// import { defineConfig } from "vite";
 
-export default defineConfig ({
+// export default defineConfig ({
+//   build: {
+//     lib: {
+//       entry: resolve(__dirname, "src/index.ts"),
+//       name: "component-library",
+   		 
+//       fileName: "index",
+//     },
+//     rollupOptions: {
+//       external: ["react"],
+//     },
+//   },
+// });
+
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+import { peerDependencies } from "./package.json";
+
+export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/index.js"),
-      name: "my-library",
-   		 
-      fileName: "index",
+      entry: "./index.ts", // Specifies the entry point for building the library.
+      name: "component-library", // Sets the name of the generated library.
+      fileName: (format) => `index.${format}.js`, // Generates the output file name based on the format.
+      formats: ["cjs", "es"], // Specifies the output formats (CommonJS and ES modules).
     },
     rollupOptions: {
-      external: ["react"],
+      external: [...Object.keys(peerDependencies)], // Defines external dependencies for Rollup bundling.
     },
+    sourcemap: true, // Generates source maps for debugging.
+    emptyOutDir: true, // Clears the output directory before building.
   },
+  plugins: [dts()], // Uses the 'vite-plugin-dts' plugin for generating TypeScript declaration files (d.ts).
 });
